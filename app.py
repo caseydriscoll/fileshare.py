c8163fdf78179d3fca73194317c8a13b67298c06
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-09-13 12:29:33 -0400

    Adds delete functionality

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
-from os import listdir
+from os import listdir, remove
 from os.path import isfile, join
 
 app = Flask(__name__)
 
 @app.route('/')
 def fileshare():
-  files = [ f for f in listdir('uploads/') if isfile(join('uploads/',f)) ]
+  files = [ f for f in listdir('files/') if isfile(join('files/',f)) ]
   return render_template('app.html', files=files)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
   f = request.files['file']
-  f.save('uploads/' + secure_filename(f.filename))
+  f.save('files/' + secure_filename(f.filename))
   return redirect(url_for('fileshare'))
 
-@app.route('/uploads/<filename>')
+@app.route('/<filename>')
 def download_file(filename):
-  return send_from_directory('uploads/', filename)
+  return send_from_directory('files/', filename)
+
+@app.route('/delete/<filename>')
+def delete_file(filename):
+  remove('files/' + filename)
+  return redirect(url_for('fileshare'))
+
 
 if __name__ == '__main__':
   app.run(debug=True)
