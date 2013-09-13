479da23c156a703583a3e1cd6e38ba08f2f08e53
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-09-13 09:09:36 -0400

    Adds ability to upload files

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def fileshare():
+  return render_template('app.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+  f = request.files['file']
+  f.save('uploads/' + secure_filename(f.filename))
   return render_template('app.html')
 
 if __name__ == '__main__':
