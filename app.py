6a9db8b405570365f690c4d02bbfe67894bc4665
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-09-13 08:25:55 -0400

    Adds template

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello World! How are ya?'
+  return render_template('app.html')
 
 if __name__ == '__main__':
   app.run('0.0.0.0')
