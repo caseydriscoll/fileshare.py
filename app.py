b4e8665e280b4b206822f7172d6036bd8b2d4f73
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-09-13 09:29:12 -0400

    Adds ability to view files

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug import secure_filename
+from os import listdir
+from os.path import isfile, join
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def fileshare():
-  return render_template('app.html')
+  files = [ f for f in listdir('uploads/') if isfile(join('uploads/',f)) ]
+  return render_template('app.html', files=files)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
   f = request.files['file']
   f.save('uploads/' + secure_filename(f.filename))
-  return render_template('app.html')
+  return redirect(url_for('fileshare'))
 
 if __name__ == '__main__':
   app.run('0.0.0.0')
